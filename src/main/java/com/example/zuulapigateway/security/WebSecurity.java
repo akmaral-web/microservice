3758338eda7a376fe010ad32c3d88c78f9c58cdd
Akmaral Shagymgereyeva <ashagimgereyeva@mail.ru>
2021-11-19 22:28:30 +0600

    Secure service-to-service

--- src/main/java/com/example/zuulapigateway/security/WebSecurity.java
+++ src/main/java/com/example/zuulapigateway/security/WebSecurity.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -36,4 +37,15 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser("rest-client")
+                .password("{noop}p@ssword")
+                .roles("REST_CLIENT");
+    }
+
 }
